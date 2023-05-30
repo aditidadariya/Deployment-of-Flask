57e6b1326d85a2aff5932d35d8a519d5c42066f7
Aditi Dadariya <103076396+aditidadariya@users.noreply.github.com>
2023-05-30 16:43:24 +0100

    Updated app.py to include predict_api

--- MLModelDeployment/app.py
+++ MLModelDeployment/app.py
@@ -30,6 +30,17 @@ def predict():
         output = "Approved"
     return render_template("index.html", prediction_text = "The predicted approval is {}".format(output))
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+    
 if __name__ == "__main__":
     flask_app.run(debug=True)
     
